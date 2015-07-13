f0bdc8b1ce508db1c66a90be519a85948ca879d2
Felix Aronsson <felix@tumblr.com>
2015-07-13 12:17:07 +0200

    Fix syslog logging module

--- tasks/modules/logging/syslog.rb
+++ tasks/modules/logging/syslog.rb
@@ -3,8 +3,8 @@ require 'syslog'
 module Logging
   module Syslog
     def self.log message
-      Syslog.open("genesis", Syslog::LOG_PID, Syslog::LOG_USER) unless Syslog.opened?
-      Syslog.log(Syslog::LOG_INFO, message)
+      ::Syslog.open("genesis", ::Syslog::LOG_PID, ::Syslog::LOG_USER) unless ::Syslog.opened?
+      ::Syslog.log(::Syslog::LOG_INFO, message)
     end
   end
 end
