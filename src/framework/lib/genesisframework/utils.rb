3902667f28bd63007a016d2ca08252fba1e5922d
Xavier Krantz <xakraz@gmail.com>
2017-06-06 15:07:34 +0200

    [Framework] Added comments in Utils

--- src/framework/lib/genesisframework/utils.rb
+++ src/framework/lib/genesisframework/utils.rb
@@ -4,16 +4,18 @@ require 'facter'
 module Genesis
   module Framework
     module Utils
-      def self.tmp_path filename, sandbox = ""
-        location = File.join(ENV['GENESIS_ROOT'], "tmp", sandbox)
-        Dir.mkdir(location, 0755) unless File.directory? location
-        File.join(location, filename)
-      end
 
       @@config_cache = Hash.new
       @@collins_conn = nil
       @@facter = nil
       @@loggers = nil
+
+      # A directory for downloaded stuff during Genesis workflow
+      def self.tmp_path filename, sandbox = ""
+        location = File.join(ENV['GENESIS_ROOT'], "tmp", sandbox)
+        Dir.mkdir(location, 0755) unless File.directory? location
+        File.join(location, filename)
+      end
 
       # mimicking rail's cattr_accessor
       def self.config_cache
@@ -26,13 +28,19 @@ module Genesis
 
       def self.collins
         if @@collins_conn.nil?
-          cfg = { :host => self.config_cache['collins']['host'], :username => self.config_cache['collins']['username'], :password => self.config_cache['collins']['password'] }
+          # http://www.rubydoc.info/gems/collins_client/0.2.17/Collins%2FClient:initialize
+          cfg = {
+            :host => self.config_cache['collins']['host'],
+            :username => self.config_cache['collins']['username'],
+            :password => self.config_cache['collins']['password']
+          }
           @@collins_conn = ::Collins::Client.new(cfg)
         end
 
         @@collins_conn
       end
 
+      # Allow usage of embedded Facts within Genesis Tasks modules
       def self.facter
         if config_cache.fetch(:facter_cache, true)
           if @@facter.nil?
@@ -44,6 +52,11 @@ module Genesis
         end
       end
 
+      # Send a message, from a task, to all loggers referenced in Genesis config
+      # file, using the specified level.
+      # @param[String] subsystem
+      # @param[String] message
+      # @param[String or Symbol] level (defaults to: nil) — severity level to use
       def self.log subsystem, message, level = nil
         logline  = subsystem.to_s + " :: " + message
 
@@ -71,8 +84,11 @@ module Genesis
       end
 
       # Copied from https://github.com/tumblr/collins/blob/master/support/ruby/collins-client/lib/collins/api/logging.rb#L155
-      # A private method used to validate the log level from user input
-      # Each Genesis_framework logger modules will have to do a mapping from Collins_client SEVERITY to their own.
+      # A method used to validate the log level from user input and returns a String
+      #
+      # Notes:
+      #   - Each Genesis_framework logger modules will have to do a mapping
+      #     from Collins_client SEVERITY to their own.
       def self.log_level_from_string level
         return nil if (level.nil? || level.empty?)
         s = Collins::Api::Logging::Severity
